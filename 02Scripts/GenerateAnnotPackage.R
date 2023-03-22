a91e044cb5bbe53f31e1107398160913af5950dc
Roxyam <roxana.andreea.moldovan@gmail.com>
2023-03-23 00:06:49 +0100

    4. Generate annotation package script creation.

---
+++ 02Scripts/GenerateAnnotPackage.R
@@ -0,0 +1,38 @@
+#!/usr/bin/env Rscript
+
+# ~~~~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~ ** ~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~~~~ #
+#
+# GenerateAnnotPackage.R   
+# 
+# Generate annotation package:
+#
+#     ...
+#
+#
+#
+#     ****************************************************************
+#     *    Author:  Roxana Andreea Moldovan                          *
+#     *    Contact: roxana.andreea.moldovan@gmail.com                *
+#     *    Version: 1.2                                              *
+#     *    Creation date: 23/03/2023                                 *
+#     ****************************************************************
+#
+#
+# ~~~~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~ ** ~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~~~~ #
+
+
+# ~~~~~~~~~~~~ Loading packages ~~~~~~~~~~~~ #
+
+library(pacman)
+pacman::p_load(AnnotationForge)
+pacman::p_load(glue)
+pacman::p_load()
+
+
+# ~~~~~~~~~~~~ Parameters ~~~~~~~~~~~~ #
+
+
+# ~~~~~~~~~~~~ Main ~~~~~~~~~~~~ #
+
+
+# ~~~~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~ ** ~~~~~~~~~ ~~~~~~~~~~~~ ~~~~~~~~~~~~ #

5c93a217d2d7571eb2102e9a1dc1cec443cd3f97
Roxyam <roxana.andreea.moldovan@gmail.com>
2023-03-22 18:46:10 +0100

    2. Download data script format.

---
+++ 02Scripts/01Download.R
@@ -0,0 +1,29 @@
+
+#!/usr/bin/env Rscript
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#
+# 01Download.R
+#
+# 
+# Data download and preparation:
+#     
+#     This script allows you to download study data from GEO and
+#     ArrayExpress using their accession numbers. It takes as input
+#     the Accession numbers for GEO and/or ArrayExpress, which can be
+#     entered as one or more values separated by commas. Additionally,
+#     you can also specify a working directory, within which a
+#     directory named Data will be created to store and process the
+#     data. If no directory is specified, the current directory will
+#     be used.
+#
+#
+#     ****************************************************************
+#     *    Author:  Roxana Andreea Moldovan                          *
+#     *    Contact: roxana.andreea.moldovan@gmail.com                *
+#     *    Version: 1.2                                              *
+#     *    Creation date: 12/02/2023                                 *
+#     ****************************************************************
+#
+#
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#

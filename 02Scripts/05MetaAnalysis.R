0a32b4a39cddef3e6e73eef29b38ac7b2ebd5a9d
Roxyam <roxana.andreea.moldovan@gmail.com>
2023-04-13 18:58:44 +0200

    Meta script version1

--- 02Scripts/05MetaAnalysis.R
+++ 02Scripts/05MetaAnalysis.R
@@ -34,7 +34,7 @@ pacman::p_load(stringr)
 
 # ~~~~~~~~~~~~ Functions ~~~~~~~~~~~~ #
 
-source("../Functions/Functions.R")
+#source("../Functions/Functions.R")
 
 RX_getSE <- function(Data # Fit limma
 ){
@@ -185,19 +185,21 @@ parser$add_argument("-re", "--redo",
 
 ## NONO
 #------------- Checking arguments 
-getwd()
+
 ### XX NONO
 args = list()
 args$studies = "ALL"
 args$tissue = "SAT"
-args$filein = "C:/Users/roxya/OneDrive/Documentos/01Master_bioinformatica/00TFM/Git/T2D-Meta-Analysis/Data/DifferentialExpressionObesity.RData"
-args$outdir = "C:/Users/roxya/OneDrive/Documentos/01Master_bioinformatica/00TFM/Git/T2D-Meta-Analysis/Data/Obesity/OFM_CMvsObF_CF"
+args$filein = "C:/Users/roxya/OneDrive/Documentos/01Master_bioinformatica/00TFM/Git/T2D-Meta-Analysis/Data/DifferentialExpressionDiabetes.RData"
+args$outdir = "C:/Users/roxya/OneDrive/Documentos/01Master_bioinformatica/00TFM/Git/T2D-Meta-Analysis/Data/Diabetes/IRM_ISMvsIRF_ISF"
+args$id = "Meta-analysis_4"
 args$prefix = "Meta-analysis_4"
 #args$contrast = "Ob - C, Ob.M - C.M, Ob.F - C.F, (Ob.M - C.M) - (Ob.F - C.F)"
-args$contrast = "(Ob.M - C.M) - (Ob.F - C.F)"
+args$contrast = "(IR.M - IS.M) - (IR.F - IS.F)"
 args$report = TRUE
 args$redo = FALSE
 args$plim = 0.05
+args$pmax = 50
 
 ## NONO
 
@@ -243,4 +245,260 @@ if(! file.exists(args$outdir)){
   system(glue("mkdir {args$outdir}"))
 }
 # Plots directory
-system(glue("mkdir PlotsDir"))
+system(glue("mkdir {PlotsDir}"))
+
+
+#------------- 1. Meta-analysis for genes
+cat("\n---------------------------------------------\n")
+cat("Analyzing...\n") 
+contrast = Contrasts[[1]]
+Mats_out = glue("{args$outdir}/{args$prefix}_mats.RData")
+Mats_in = glue("{args$outdir}/{args$id}_mats.RData")
+MA_out = glue("{args$outdir}/{args$prefix}_MA.RData")
+MA_in = glue("{args$outdir}/{args$id}_MA.RData")
+DF_out = glue("{args$outdir}/{args$prefix}_DF.RData")
+DF_in = glue("{args$outdir}/{args$prefix}_DF.RData")
+
+if(file.exists(Mats_in) & isFALSE(args$redo)){
+  # Load previous data
+  load(file=Mats_in)
+}else{
+  Mats = RX_MetaAnalysis_prep(Studies = Studies,
+                              Tissue = Tissue,
+                              Datas = Datas,
+                              SE_coef = "coefSE",
+                              Contrast = contrast)
+  save(Mats, file = Mats_out)
+}
+
+if(file.exists(MA_in) & isFALSE(args$redo)){
+  # Load previous data
+  load(file=MA_in)
+}else{
+  #------------- 1. Meta-analysis for genes  
+  MA <- sapply(rownames(Mats$MatLogFC),
+               function(x) metafor::rma(yi = Mats$MatLogFC[x, ],
+                                        sei = Mats$MatSE[x, ],
+                                        method = method,
+                                        slab = Studies,
+                                        verbose = FALSE), simplify = FALSE, USE.NAMES = TRUE ) 
+  save(MA, file = MA_out)
+}
+
+if(file.exists(DF_in) & isFALSE(args$redo)){
+  # Load previous data
+  load(file=DF_in)
+}else{
+  # Data frame including the detailed results.
+  resultMA <- as.data.frame(do.call("rbind",
+                                    sapply(MA,
+                                           function(x){c("interv_inf" = x$ci.lb, 
+                                                         "Summary_logFC" = x$b, 
+                                                         "interv_sup" = x$ci.ub,
+                                                         "pvalue" = x$pval,
+                                                         x$QE,x$QEp, x$se,
+                                                         x$tau2, x$I2, x$H2)},
+                                           simplify = FALSE)))
+  # Adjust p.values
+  resultMA$p.adjust.fdr <- stats::p.adjust(resultMA$pvalue, method = "fdr")
+  resultMA$p.adjust.BY  <- stats::p.adjust(resultMA$pvalue, method = "BY")
+  resultMA$logFDR <- -log10(resultMA$p.adjust.fdr)
+  resultMA$N <- rowSums(!is.na(Mats$MatSE))
+  save(resultMA, file = DF_out)
+}
+
+
+sigData = resultMA[resultMA[, "p.adjust.fdr"] < args$plim,]
+sigData = sigData[order(abs(sigData[, "p.adjust.fdr"])),]
+if (nrow(sigData > args$pmax )){
+  sigData = sigData[c(1:args$pmax),]
+}
+
+color = "#2494b5"
+  for (gene in rownames(sigData)){
+    res <- MA[[gene]]
+    #res <- rma(yi= Mats$MatLogFC[gene,], sei =Mats$MatSE[gene,], method = "DL", slab = Studies)
+    # Forest plot
+    svg(glue("{PlotsDir}/{args$prefix}_{gene}forest.svg"))
+    forest(res, 
+           slab = toupper(colnames(Mats$MatLogFC)),
+           xlab="Fold Change", cex=0.7,
+           mlab="Modelo 'DL' para todos los estudios.", col = color, 
+           main = paste("\n", Mats$Anot[gene, "SYMBOL"] , " (", gene, ")",sep=""))    
+    text( 9,-3, "logFC [IC 95%]", pos=2, cex = 0.7)
+    dev.off()
+    
+    #Funnel plot
+    svg(glue("{PlotsDir}/{args$prefix}_{gene}funnel.svg"))
+    funnel(res, main=paste("\n", Mats$Anot[gene, "SYMBOL"] , " (", gene, ")",sep=""), back ="transparent", ylab = "Error estándar",
+           xlab = "logFC", label = "out", shade=c("#F0EFEF", "#90C432", "#2494b5"), refline=0,
+           level=c(90, 95, 99), legend=TRUE, atransf=exp)
+    dev.off()  
+    
+    #Influence plot
+    svg(glue("{PlotsDir}/{args$prefix}_{gene}influence.svg"))
+    inf <- influence(res)
+    plot(inf)
+    dev.off()  
+  }
+
+#------------- Report
+
+report_out = glue("{args$outdir}/{args$prefix}Report.rmd")
+if(isTRUE(args$report)){
+  # File info
+  cat('---
+title: "05 Metaanálisis."
+author: "Roxana Andreea Moldovan Moldovan"
+date: "`r Sys.Date()`"
+output:
+  html_document:
+    toc: false
+    toc_float: false
+    number_sections: false 
+    code_folding: hide
+    theme: flatly
+    bg: "#FFFFFF"
+    fg: "#202020"
+    primary: "#90C432"
+    secondary: "#2494b5" 
+  pDT_document:
+    extra_dependencies: "subfig"
+linkcolor: blue
+urlcolor: blue
+citecolor: blue
+header-includes:
+- \\usepackage{subfig}
+- \\usepackage{float}
+fig_caption: yes 
+link-citations: TRUE 
+---
+        ',
+      sep ="",
+      file = report_out,
+      append = FALSE
+  )
+  
+  ## Meta analisis
+  cat('\n## Meta-análisis {.tabset .tabset-fade -} \n',
+      '&nbsp;  \n\n',
+      sep ="",
+      file = report_out,
+      append = TRUE)
+  
+  # Load packages
+  cat(
+    # Open chunk
+    '\n```{r echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE}\n',
+    '# Load packages \n',
+    'library(pacman)
+pacman::p_load(glue)
+pacman::p_load(DT)
+pacman::p_load(magick)',
+    # Close chunk
+    '\n```  \n\n',
+    sep ="",
+    file = report_out,
+    append = TRUE)
+  
+  cat('\nEn este documento se muestran los resultados del metaanálisis de los estudios: ', 
+      paste(Studies, collapse = ", "),
+      ' para el contraste ',
+      args$contrast, 
+      '.  \n&nbsp;  \n\n',
+      sep ="",
+      file = report_out,
+      append = TRUE)
+  
+  cat('\n### Tabla de resultados {-} \n',
+      '&nbsp;  \n\n',
+      sep ="",
+      file = report_out,
+      append = TRUE)
+  
+  cat(
+    # Open chunk
+    '\n```{r echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE}\n',
+    'DT = get(load("', DF_out, '"))\n',
+    'datatable(DT, options = list(caption = "Resultados del metaanálisis.",
+                              scrollX = TRUE))',
+    # Close chunk
+    '\n```  \n\n','\n&nbsp;  \n\n',
+    sep ="",
+    file = report_out,
+    append = TRUE)
+  
+  cat('\n### Representación gráfica {-}  \n&nbsp; \n',
+      '\nAquí se muestran las figuras características del metaanálisis para los
+genes significativos tomando un p valor ajustado por BH < ', args$plim, '.  \n',
+      '&nbsp;  \n\n',
+      sep ="",
+      file = report_out,
+      append = TRUE)
+  
+  # Function
+  cat(
+    # Open chunk
+    '\n```{r echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE}\n',
+    '# Función para repesentar las figuras\n',
+    'grid_img <- function(list_fig, dir, patt, dim = 300){
+  # Abrimos todas las filas:
+  forest_plots = c()
+  funnel_plots = c()
+  influ_plots = c()
+  for (fig in list_fig){
+    f1 = glue("{fig}forest.svg")
+    out1 <- magick::image_read(paste0(dir, "/", f1))
+    forest_plots = c(forest_plots, image_scale(out1, dim))
+    
+    f2 = glue("{fig}funnel.svg")
+    out2 <- magick::image_read(paste0(dir,"/", f2))
+    funnel_plots = c(funnel_plots, image_scale(out2, dim))  
+    
+    f3 = glue("{fig}influence.svg")
+    out3 <- magick::image_read(paste0(dir,"/", f3))
+    influ_plots = c(influ_plots, image_scale(out3, dim))
+  } 
+  
+  lim = length(forest_plots)
+  grid = NULL
+  for ( i in seq(list_fig)){
+    a = forest_plots[[i]]
+    b = funnel_plots[[i]]
+    c = influ_plots[[i]]
+    
+    fila <- image_append(c(a, b, c))
+    
+    if(is.null(grid)){
+      grid = fila
+    }else{
+      grid = image_append(c(grid,fila),  stack = TRUE)
+    } 
+  }
+    return(grid)
+}',
+    # Close chunk
+    '\n```  \n\n','\n&nbsp;  \n\n',
+    sep ="",
+    file = report_out,
+    append = TRUE)
+}
+
+cat(
+  # Open chunk
+  '\n```{r echo=TRUE, eval=TRUE, message=FALSE, warning=FALSE, echo=FALSE, out.width=".99\\\\linewidth"}\n',
+  'path = "',PlotsDir, '"\n',
+  'files <- list.files(path = path, pattern = "',glue("{args$prefix}.*forest"),'")\n',
+  'figuras = sapply( files, function(x) substr(x, start = 0, stop = nchar(x)-10), 
+                  USE.NAMES = FALSE)\n',
+  '# Plot\n',
+  'grid_img(figuras, dir = path)',
+  # Close chunk
+  '\n```  \n\n','\n&nbsp;  \n\n',
+  sep ="",
+  file = report_out,
+  append = TRUE)
+
+
+# Create HTML
+rmarkdown::render(report_out)
